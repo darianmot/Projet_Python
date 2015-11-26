af7f95d1af8285d7ed0ce78746cb36675a8d393d
darianmot <darian.motamed@gmail.com>
2015-11-26 21:32:46 +0100

    Fenetre principale :     -création du tableau selon la taille de l'écran     -indexation des colonnes selon l'alphabet (infini)

---
+++ visu/columns_labels.py
@@ -0,0 +1,25 @@
+#Ici, on genère le titre des colonnes à la exce, avec un alphabet quelxonque
+
+import string
+
+
+ALPHABET=string.ascii_uppercase
+
+def add(labels,n): #Ajoute n labels a la liste labels
+    for _ in range(n):
+        k=0 if len(labels)==0 else len(labels)
+        if k<len(ALPHABET):
+            label=ALPHABET[k]
+        else:
+            label=labels[k//26-1]+ALPHABET[k%26]
+        labels.append(label)
+
+def generate(n): #Henere une liste de  labels
+    l=[]
+    add(l,n)
+    return l
+
+
+
+
+
--- visu/mainwindow.py
+++ visu/mainwindow.py
@@ -1,97 +1,107 @@
-#Generé automatiquement via pyuic5, avec un code qui le lance à la fin
-#SEULs CHANGEMENTs : taille de la fenetre + ajouts de cases (cf plus bas)
-
+# ATTENTION : vu qu'on a modifié ce fichier, mainwindow.ui est devenu obsolète
 from PyQt5 import QtCore, QtGui, QtWidgets
+import columns_labels
+CELLWIDTH=100
+CELLHEIGHT=30
+
+
 
 class Ui_MainWindow(object):
+
+
     def setupUi(self, MainWindow):
+        screen = QtWidgets.QDesktopWidget()
+        verticalCellNumber=int(2*screen.height()/CELLHEIGHT)
+        horizontalCellNumber=int(2*screen.width()/CELLWIDTH)
+
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(659, 356)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
         self.verticalLayout.setObjectName("verticalLayout")
         self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
         self.tableWidget.setObjectName("tableWidget")
-        self.tableWidget.setColumnCount(20)  #ajouts de colonnes
-        self.tableWidget.setRowCount(30)     #ajouts de lignes
-        item = QtWidgets.QTableWidgetItem()
-        self.tableWidget.setVerticalHeaderItem(0, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableWidget.setVerticalHeaderItem(1, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableWidget.setVerticalHeaderItem(2, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableWidget.setHorizontalHeaderItem(0, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableWidget.setHorizontalHeaderItem(1, item)
+
+        self.tableWidget.setColumnCount(horizontalCellNumber)
+
+        self.tableWidget.setRowCount(verticalCellNumber)
+
+        for k in range(verticalCellNumber):                         #On attribue un identifiant à chaques colonnes
+            item = QtWidgets.QTableWidgetItem()
+            self.tableWidget.setVerticalHeaderItem(k,item)
+
+        for k in range(horizontalCellNumber):                       #On attribue un identifiant à chaques lignes
+            item = QtWidgets.QTableWidgetItem()
+            self.tableWidget.setHorizontalHeaderItem(k, item)
+
         self.verticalLayout.addWidget(self.tableWidget)
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 659, 25))
-        self.menubar.setObjectName("menubar")
-        self.menuFichier = QtWidgets.QMenu(self.menubar)
-        self.menuFichier.setObjectName("menuFichier")
-        self.menuEdition = QtWidgets.QMenu(self.menubar)
-        self.menuEdition.setObjectName("menuEdition")
-        self.menuGrosse_bite = QtWidgets.QMenu(self.menuEdition)
-        self.menuGrosse_bite.setObjectName("menuGrosse_bite")
-        self.menuPetite_bite = QtWidgets.QMenu(self.menuEdition)
-        self.menuPetite_bite.setObjectName("menuPetite_bite")
-        MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
         self.toolBar = QtWidgets.QToolBar(MainWindow)
         self.toolBar.setObjectName("toolBar")
         MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar)
+        #Le menu
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setObjectName("menubar")
+        self.menuFichier = QtWidgets.QMenu(self.menubar)
+        self.menuFichier.setObjectName("menuFichier")
+        self.menuSsmenu1 = QtWidgets.QMenu(self.menuFichier)
+        self.menuSsmenu1.setObjectName("menuSsmenu1")
+        self.menuSsmenu2 = QtWidgets.QMenu(self.menuFichier)
+        self.menuSsmenu2.setObjectName("menuSsmenu2")
+        MainWindow.setMenuBar(self.menubar)
         self.actionOuvrir = QtWidgets.QAction(MainWindow)
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap("stremio.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.actionOuvrir.setIcon(icon)
         self.actionOuvrir.setObjectName("actionOuvrir")
-        self.actionDarian = QtWidgets.QAction(MainWindow)
-        self.actionDarian.setObjectName("actionDarian")
-        self.actionAnas = QtWidgets.QAction(MainWindow)
-        self.actionAnas.setObjectName("actionAnas")
-        self.actionAtime = QtWidgets.QAction(MainWindow)
-        self.actionAtime.setObjectName("actionAtime")
-        self.menuGrosse_bite.addAction(self.actionDarian)
-        self.menuPetite_bite.addAction(self.actionAnas)
-        self.menuPetite_bite.addAction(self.actionAtime)
-        self.menuEdition.addAction(self.menuGrosse_bite.menuAction())
-        self.menuEdition.addAction(self.menuPetite_bite.menuAction())
-        self.menubar.addAction(self.menuFichier.menuAction())
-        self.menubar.addAction(self.menuEdition.menuAction())
+        self.actionAction1 = QtWidgets.QAction(MainWindow)
+        self.actionAction1.setObjectName("actionAction1")
+        self.actionAction2 = QtWidgets.QAction(MainWindow)
+        self.actionAction2.setObjectName("actionAction2")
+        self.actionAction2_1 = QtWidgets.QAction(MainWindow)
+        self.actionAction2_1.setObjectName("actionAction2_1")
         self.toolBar.addAction(self.actionOuvrir)
         self.toolBar.addSeparator()
+        self.menuSsmenu1.addAction(self.actionAction1)
+        self.menuSsmenu1.addAction(self.actionAction2)
+        self.menuSsmenu2.addAction(self.actionAction2_1)
+        self.menuFichier.addAction(self.menuSsmenu1.menuAction())
+        self.menuFichier.addAction(self.menuSsmenu2.menuAction())
+        self.menubar.addAction(self.menuFichier.menuAction())
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
+        screen = QtWidgets.QDesktopWidget()
+        verticalCellNumber=int(2*screen.height()/CELLHEIGHT)
+        horizontalCellNumber=int(2*screen.width()/CELLWIDTH)
+
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        item = self.tableWidget.verticalHeaderItem(0)
-        item.setText(_translate("MainWindow", "Nouvelle ligne"))
-        item = self.tableWidget.verticalHeaderItem(1)
-        item.setText(_translate("MainWindow", "Nouvelle ligne"))
-        item = self.tableWidget.verticalHeaderItem(2)
-        item.setText(_translate("MainWindow", "Nouvelle ligne"))
-        item = self.tableWidget.horizontalHeaderItem(0)
-        item.setText(_translate("MainWindow", "Nouvelle colonne"))
-        item = self.tableWidget.horizontalHeaderItem(1)
-        item.setText(_translate("MainWindow", "Nouvelle colonne"))
-        self.menuFichier.setTitle(_translate("MainWindow", "Fichier"))
-        self.menuEdition.setTitle(_translate("MainWindow", "Bite"))
-        self.menuGrosse_bite.setTitle(_translate("MainWindow", "Grosse bite"))
-        self.menuPetite_bite.setTitle(_translate("MainWindow", "Petites bites"))
+        columnsLabels=columns_labels.generate(horizontalCellNumber)
+
+        for k in range(1,self.tableWidget.rowCount()+1):            #On renomme chaques colonnes
+            item=self.tableWidget.verticalHeaderItem(k-1)
+            item.setText(_translate("MainWindow",str(k)))
+
+        for k in range(1,self.tableWidget.columnCount()+1):         #On renomme chaques lignes
+            item = self.tableWidget.horizontalHeaderItem(k-1)
+            item.setText(_translate("MainWindow", columnsLabels[k-1]))
+
         self.toolBar.setWindowTitle(_translate("MainWindow", "toolBar"))
+        self.menuFichier.setTitle(_translate("MainWindow", "Menu1"))
+        self.menuSsmenu1.setTitle(_translate("MainWindow", "ssmenu1"))
+        self.menuSsmenu2.setTitle(_translate("MainWindow", "ssmenu2"))
         self.actionOuvrir.setText(_translate("MainWindow", "Ouvrir"))
         self.actionOuvrir.setToolTip(_translate("MainWindow", "infobulle"))
-        self.actionDarian.setText(_translate("MainWindow", "Darian"))
-        self.actionAnas.setText(_translate("MainWindow", "Anas"))
-        self.actionAtime.setText(_translate("MainWindow", "Atime"))
+        self.actionAction1.setText(_translate("MainWindow", "action1"))
+        self.actionAction2.setText(_translate("MainWindow", "action2"))
+        self.actionAction2_1.setText(_translate("MainWindow", "action21"))
+
 
 
 if __name__ == "__main__":
@@ -100,6 +110,7 @@ if __name__ == "__main__":
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
-    MainWindow.showMaximized() #Maximise la fenetre a l'ouverture
+    MainWindow.showMaximized() #Pour agrandir au max la fenetre
     sys.exit(app.exec_())
+
 
