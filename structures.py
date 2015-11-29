bb5e7bce3aec205b71d1c38ad6f8ca70947e42cb
Darian <darian.motamed@gmail.com>
2015-11-29 21:44:29 +0100

    Propositions de structures pour representer l'ensemble des cellules

--- structures.py
+++ structures.py
@@ -1,10 +1,77 @@
 import visu.columns_labels as columns_labels
-class Cell (object): #caractéristiques et organisation d'une cellule
 
+class Cell(object): #caractéristiques et organisation d'une cellule
     def __init__(self,X,Y,value):
         self.X = X
         self.Y = Y
         self.value = value
-        self.name = str(columns_labels.generate(Y+1)[-1]) + str(X+1)     #chaine de caractères
+        self.name = None     #chaine de caractères
         self.neighbours = []             #liste des cellules dépendant de celle-ci
 
+    def __repr__(self):
+        return str(self.name)
+
+
+class reseau(object):  #On classe par name
+    def __init__(self):
+        self.labels=columns_labels.generate(1)
+        self.dict={self.labels[0]+str(1):Cell(1,1,None)}
+        self.rows=1
+        self.columns=1
+
+    def addCell(self,x,y,name):
+        self.dict[name]=Cell(x,y,None)
+
+    def addRow(self):
+        for c in range(self.columns):
+            self.addCell(c+1,self.rows+1,self.labels[c]+str(self.rows+1))
+        self.rows+=1
+
+    def addRows(self,n):
+        for _ in range(n):
+            self.addRow()
+
+    def addColumn(self):
+        columns_labels.add(self.labels,1)
+        for r in range(self.rows):
+            self.addCell(self.columns+1,r+1,self.labels[self.columns]+str(r+1))
+        self.columns+=1
+
+    def addColumns(self,n):
+        for _ in range(n):
+            self.addColumn()
+
+
+
+class reseau2(object): #On classe par coordonnées
+    def __init__(self):
+        self.labels=columns_labels.generate(1)
+        self.matrix=[[Cell(1,1,None)]]
+        self.columnNumber=1
+        self.rowNumber=1
+
+    def addRow(self):
+        l=[]
+        for c in range(self.columnNumber):
+            l.append(Cell(c+1,self.rowNumber+1,None))
+            l[-1].name=self.labels[c]+str(self.rowNumber)
+        self.matrix.append(l)
+        self.rowNumber+=1
+
+    def addRows(self,n):
+        for _ in range(n):
+            self.addRow()
+
+    def addColumn(self):
+        columns_labels.add(self.labels,1)
+        for r in range(self.rowNumber):
+            self.matrix[r].append(Cell(self.columnNumber+1,r+1,None))
+            self.matrix[r][-1].name=self.labels[self.columnNumber]+str(r)
+        self.columnNumber+=1
+
+    def addColumns(self,n):
+        for _ in range(n):
+            self.addColumn()
+
+    def __repr__(self):
+        return str(self.matrix)
