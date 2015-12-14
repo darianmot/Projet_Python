5b21041e75cc7abc84eb66978462569e93110256
atime <atime.ronda@gmail.com>
2015-12-14 16:37:33 +0100

    #Fenêtre d'ouverture complètement remaniée. Ressemble maintenant à la fenêtre d'ouverture classique type office (pas la mocheté qu'anas nous a pondue au début).
    A FAIRE: connecter le open à la fermeture de la fenêtre

--- visu/registerwindow.py
+++ visu/registerwindow.py
@@ -14,32 +14,42 @@ def Quit():
 class UI_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(605, 274)
+        MainWindow.resize(650, 440)
         MainWindow.setMaximumSize(QtCore.QSize(10000, 10000))
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(490, 186, 85, 61))
-        self.pushButton.setObjectName("pushButton")
+        # self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        # self.pushButton.setGeometry(QtCore.QRect(490, 186, 85, 61))
+        # self.pushButton.setObjectName("pushButton")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(80, 170, 361, 41))
+        self.label.setGeometry(QtCore.QRect(80, 170, 361, 400))
         self.label.setObjectName("label")
-        self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
-        self.textBrowser.setGeometry(QtCore.QRect(20, 0, 571, 171))
-        self.textBrowser.setObjectName("textBrowser")
-        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit.setGeometry(QtCore.QRect(10, 200, 401, 51))
-        self.textEdit.setObjectName("textEdit")
+
+
+
+        self.arbo_layout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.tree = QtWidgets.QFileDialog()
+        self.arbo_layout.addWidget(self.tree)
+
+
+
+
+
+
+
+        # self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        # self.textEdit.setGeometry(QtCore.QRect(10, 200, 401, 51))
+        # self.textEdit.setObjectName("textEdit")
         self.retranslateUi(MainWindow)
-        self.pushButton.pressed.connect(Quit)
+        #self.pushButton.pressed.connect(Quit)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Open..."))
-        self.pushButton.setText(_translate("MainWindow", "Validate"))
-        self.label.setText(_translate("MainWindow", "Writte down the adress of the file"))
+        #self.pushButton.setText(_translate("MainWindow", "Validate"))
+        #self.label.setText(_translate("MainWindow", "Writte down the adress of the file"))
 
 if __name__ == "__main__":
     import sys
