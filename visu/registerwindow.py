08259f25d7f9e03f2be20748415d4b3205e16174
AtimeRonda <atime.ronda@gmail.com>
2015-12-15 19:15:30 +0100

    Nettoyage du code de la registerwindow

--- visu/registerwindow.py
+++ visu/registerwindow.py
@@ -6,46 +6,32 @@
 #      by: PyQt5 UI code generator 5.2.1
 #
 # WARNING! All changes made in this file will be lost!
-import recOrd,sys
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore, QtWidgets
 
 
 class UI_MainWindow(object):
     def setupUi(self, MainWindow):
+        def Quit():
+            MainWindow.close()
+
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(650, 440)
         MainWindow.setMaximumSize(QtCore.QSize(10000, 10000))
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        # self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        # self.pushButton.setGeometry(QtCore.QRect(490, 186, 85, 61))
-        # self.pushButton.setObjectName("pushButton")
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(80, 170, 361, 400))
         self.label.setObjectName("label")
-
-
-
         self.arbo_layout = QtWidgets.QVBoxLayout(self.centralwidget)
         self.tree = QtWidgets.QFileDialog()
         self.arbo_layout.addWidget(self.tree)
 
 
-
-
-
-
-
-        # self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
-        # self.textEdit.setGeometry(QtCore.QRect(10, 200, 401, 51))
-        # self.textEdit.setObjectName("textEdit")
-        self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
-        self.tree.fileSelected.connect(MainWindow.close)
+        self.tree.fileSelected.connect(Quit)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Open..."))
-        #self.pushButton.setText(_translate("MainWindow", "Validate"))
-        #self.label.setText(_translate("MainWindow", "Writte down the adress of the file"))
+
 
