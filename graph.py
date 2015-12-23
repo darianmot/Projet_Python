cff8ee2ff553517733069213dbf46805341dfdeb
insaf <anas.darwich@aliceadsl.fr>
2015-12-24 00:09:51 +0100

    #fenetre des graphes en cours de constructions, j'ai toujours pas vu SW7:le reveil de la force..

--- graph.py
+++ graph.py
@@ -1,5 +1,6 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+
 import matplotlib.pyplot as plt
+from PyQt5 import QtCore, QtGui, QtWidgets
 def graph(listeValue,ordonnée,abscisse,title,xmin,xmax,ymin,ymax,color):
     newliste=[x.value for x in listeValue]
     plt.plot[newliste,color]
@@ -10,42 +11,157 @@ def graph(listeValue,ordonnée,abscisse,title,xmin,xmax,ymin,ymax,color):
     plt.show()
 
 
-# a=input('image')
-# print('a')
-# class Ui_MainWindow(object):
-#     def setupUi(self, MainWindow):
-#         MainWindow.setObjectName("graphs")
-#         MainWindow.resize(605, 400)
-#         MainWindow.setMaximumSize(QtCore.QSize(10000, 10000))
-#         #layout
-#         self.mylayout=QtWidgets.QVBoxLayout()
-#         self.mylayout.setObjectName('mylayout')
-#         self.mysecondlayout=QtWidgets.QVBoxLayout()
-#         self.mysecondlayout.setObjectName('mysecondlayout')
-#         #La liste des graphiques
-#         self.listgraph = QtWidgets.QListWidget(MainWindow)
-#         self.listgraph.setMinimumSize(QtCore.QSize(300, 10000))
-#         self.listgraph.setSizePolicy(QtWidgets.QSizePolicy.Fixed,QtWidgets.QSizePolicy.Fixed)
-#         self.listgraph.setObjectName("Graphiques")
-#         self.mysecondlayout.addWidget(self.listgraph)
-#         #la description
-#         self.descriptionimage=QtWidgets.QLabel(MainWindow)
-#         self.descriptionimage.setMinimumSize(QtCore.QSize(1000,300))
-#         pixmap=QtGui.QPixmap(a)
-#         self.descriptionimage.setPixmap(pixmap)
-#         self.mylayout.addWidget(self.descriptionimage)
-#
-#     def retranslateUi(self, MainWindow):
-#         _translate = QtCore.QCoreApplication.translate
-#         MainWindow.setWindowTitle(_translate("graphs", "graphs"))
-#
-# if __name__ == "__main__":
-#     import sys
-#     app = QtWidgets.QApplication(sys.argv)
-#     MainWindow = QtWidgets.QMainWindow()
-#     ui = Ui_MainWindow()
-#     ui.setupUi(MainWindow)
-#     MainWindow.show()
-#     sys.exit(app.exec_())
-#
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(787, 671)
+
+        #création de la grille
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+
+        # 1introduction de layout dans la grille
+        self.horizontalLayout_5 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_5.setObjectName("horizontalLayout_5")
+
+        #liste des types de graphiques
+        self.listView = QtWidgets.QListWidget(self.centralwidget)
+        self.listView.setObjectName("listView")
+        courbe=QtWidgets.QListWidgetItem()
+        self.listView.addItem(courbe)
+        courbe.setText('courbe')
+        self.histogramme=QtWidgets.QListWidgetItem()
+        self.listView.addItem(self.histogramme)
+        self.histogramme.setText('histogramme')
+        self.camembert=QtWidgets.QListWidgetItem()
+        self.listView.addItem(self.camembert)
+        self.camembert.setText('camembert')
+        #print(self.camembert.text())
+        #autre layout
+        self.horizontalLayout_5.addWidget(self.listView)
+
+        #images graphique et layout
+        self.graphicsView = QtWidgets.QGraphicsView(self.centralwidget)
+        self.graphicsView.setObjectName("graphicsView")
+        self.horizontalLayout_5.addWidget(self.graphicsView)
+        self.gridLayout.addLayout(self.horizontalLayout_5, 0, 0, 1, 2)
+        self.horizontalLayout_4 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_4.setObjectName("horizontalLayout_4")
+
+        #les labels et ligne d'éditions pour le titre
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setText("")
+        self.label_3.setObjectName("label_3")
+        self.horizontalLayout_4.addWidget(self.label_3)
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setObjectName("label_4")
+        self.horizontalLayout_4.addWidget(self.label_4)
+        self.lineEdit_3 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.horizontalLayout_4.addWidget(self.lineEdit_3)
+        self.gridLayout.addLayout(self.horizontalLayout_4, 1, 0, 1, 1)
+
+        #le scrolling pour la couleur
+        self.scrollArea = QtWidgets.QScrollArea(self.centralwidget)
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setObjectName("scrollArea")
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 379, 76))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.gridLayout.addWidget(self.scrollArea, 1, 1, 2, 1)
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+
+        #labels et layouts pour l'axe des abscisses
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setObjectName("label_2")
+        self.horizontalLayout_3.addWidget(self.label_2)
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.horizontalLayout_3.addWidget(self.lineEdit_2)
+        self.gridLayout.addLayout(self.horizontalLayout_3, 2, 0, 2, 1)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.label_5 = QtWidgets.QLabel(self.centralwidget)
+        self.label_5.setObjectName("label_5")
+        self.horizontalLayout.addWidget(self.label_5)
+
+        #les xmin xmax  dans la spin box avec les layout associées label associées
+        self.doubleSpinBox = QtWidgets.QDoubleSpinBox(self.centralwidget)
+        self.doubleSpinBox.setObjectName("doubleSpinBox")
+        self.horizontalLayout.addWidget(self.doubleSpinBox)
+        self.label_6 = QtWidgets.QLabel(self.centralwidget)
+        self.label_6.setObjectName("label_6")
+        self.horizontalLayout.addWidget(self.label_6)
+        self.doubleSpinBox_2 = QtWidgets.QDoubleSpinBox(self.centralwidget)
+        self.doubleSpinBox_2.setObjectName("doubleSpinBox_2")
+        self.horizontalLayout.addWidget(self.doubleSpinBox_2)
+        self.gridLayout.addLayout(self.horizontalLayout, 3, 1, 1, 1)
+
+        #label axe des ordonnees
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setObjectName("label")
+        self.horizontalLayout_2.addWidget(self.label)
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setObjectName("lineEdit")
+        self.horizontalLayout_2.addWidget(self.lineEdit)
+        self.gridLayout.addLayout(self.horizontalLayout_2, 4, 0, 1, 1)
+        self.horizontalLayout_6 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_6.setObjectName("horizontalLayout_6")
+        self.label_7 = QtWidgets.QLabel(self.centralwidget)
+        self.label_7.setObjectName("label_7")
+        self.horizontalLayout_6.addWidget(self.label_7)
+
+        #valeur des ordonnées et layout associée
+        self.doubleSpinBox_3 = QtWidgets.QDoubleSpinBox(self.centralwidget)
+        self.doubleSpinBox_3.setObjectName("doubleSpinBox_3")
+        self.horizontalLayout_6.addWidget(self.doubleSpinBox_3)
+        self.label_8 = QtWidgets.QLabel(self.centralwidget)
+        self.label_8.setObjectName("label_8")
+        self.horizontalLayout_6.addWidget(self.label_8)
+        self.doubleSpinBox_4 = QtWidgets.QDoubleSpinBox(self.centralwidget)
+        self.doubleSpinBox_4.setObjectName("doubleSpinBox_4")
+        self.horizontalLayout_6.addWidget(self.doubleSpinBox_4)
+        self.gridLayout.addLayout(self.horizontalLayout_6, 4, 1, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        self.buttonBox = QtWidgets.QDialogButtonBox(self.centralwidget)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setObjectName("buttonBox")
+        self.gridLayout.addWidget(self.buttonBox, 5, 0, 1, 1)
+        MainWindow.setStatusBar(self.statusbar)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label_4.setText(_translate("MainWindow", "     Title"))
+        self.label_2.setText(_translate("MainWindow", "     X axis title"))
+        self.label_5.setText(_translate("MainWindow", "xmin"))
+        self.label_6.setText(_translate("MainWindow", "xmax"))
+        self.label.setText(_translate("MainWindow", "    Y axis title"))
+        self.label_7.setText(_translate("MainWindow", "ymin"))
+        self.label_8.setText(_translate("MainWindow", "ymax"))
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
+
+
+
 
